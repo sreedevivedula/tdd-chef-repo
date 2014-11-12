8ee5c9c4f2888caac680231008286496b765c205
sree1127 <sreedevi.vedula@RACKSPACE.COM>
2014-11-12 10:32:54 +0530

    Added Infrastructure code

---
+++ cookbooks/mysql/recipes/install.rb
@@ -0,0 +1,53 @@
+#
+# Cookbook Name:: mysql
+# Recipe:: server
+#
+# Copyright 2008-2013, Chef Software, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+if platform?('ubuntu')
+  template 'mysql.seed' do
+    source 'mysql.preseed.erb'
+    path node[:temp_dir]+'/mysql.seed'
+    variables({
+                  :admin_password => node[:admin_password]
+              })
+    mode '0755'
+    owner 'root'
+    group 'root'
+  end
+
+  execute 'Set MySQL Preseed Selections' do
+    command 'debconf-set-selections ' + node[:temp_dir] + '/mysql.seed'
+  end
+
+  execute 'apt-get update' do
+    command 'sudo apt-get update'
+  end
+
+  apt_package 'mysql-server' do
+    action :install
+  end
+
+elsif platform?('centos')
+
+  yum_package 'mysql-server' do
+    action :install
+  end
+
+  service 'mysqld' do
+    action :start
+  end
+
+end
